c359be566ee46aa374d50450a2a6b30a42ffceef
Lang Towl <langtowl@gmail.com>
2025-01-28 17:20:45 -0500

    Almost working label preprocessor, known issue: the reformated values are not outputting properly, work needed

---
+++ config/class_labels.py
@@ -0,0 +1,11 @@
+class_labels = {
+    "Car": 0,
+    "Van": 1,
+    "Truck": 2,
+    "Pedestrian": 3,
+    "Person_sitting": 4,
+    "Cyclist": 5,
+    "Tram": 6,
+    # "Misc": 7,
+    # "DontCare": 8
+}
